47e245a5b69379ab48c10cb0586a1f18910a83a0
Parth-hub-sys <ptarsariya176@gmail.com>
2025-07-03 18:03:42 +0530

    add

--- deep learning/deep nural networks/store_sales_forecast_app/server/test_api.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-data = {
-    "store_nbr": 1,
-    "family": "GROCERY I",
-    "days": 7
-}
-response = requests.post("http://127.0.0.1:5000/api/predict", json=data)
-print(response.json())
